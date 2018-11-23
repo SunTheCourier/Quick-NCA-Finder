3b0b0e230e5872ef79052c317284f0dfed1a6f17
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-11-23 12:25:37 -0800

    Support dumping all NCAs

--- Program.cs
+++ Program.cs
@@ -14,21 +14,11 @@ namespace Quick_NCA_Finder
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: Quick-NCA-Finder.exe {folder to search, make this is the root of the NAND partition or SD} {TID to search for}");
+                Console.WriteLine("Usage: Quick-NCA-Finder.exe {folder to search, make this is the root of the NAND partition or SD} {TID to search for (use * for all all)}");
                 return;
             }
 
             DirectoryInfo dir = new DirectoryInfo(args[0]);
-            try
-            { 
-                TID = ulong.Parse(args[1], NumberStyles.HexNumber);
-            }
-            catch
-            {
-                Console.WriteLine("The inputted TID is not valid");
-                return;
-            }
-
             FileInfo prodkeys = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".switch/prod.keys"));
             FileInfo titlekeys = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".switch/title.keys"));
 
@@ -42,8 +32,40 @@ namespace Quick_NCA_Finder
             keys = ExternalKeys.ReadKeyFile(prodkeys.FullName, titlekeys.FullName);
             SwitchFs fs = new SwitchFs(keys, new FileSystem(dir.FullName));
 
-           foreach (KeyValuePair<ulong, Title> kv in fs.Titles)
-           {
+            if (args[1] == "*")
+            {
+                foreach (KeyValuePair<ulong, Title> kv in fs.Titles)
+                {
+                    ulong titleId = kv.Key;
+                    Title title = kv.Value;
+                    DirectoryInfo titleRoot = new DirectoryInfo($"./NCAs/{titleId:X8}");
+
+                    foreach (Nca nca in title.Ncas)
+                    {
+                        Console.WriteLine($"Saving {titleId:X8}");
+                        titleRoot.Create();
+                        FileInfo ncainfo = titleRoot.GetFile($"{nca.Header.ContentType.ToString()}.nca");
+                        using (Stream source = nca.GetStream())
+                        using (FileStream dest = ncainfo.Create())
+                        {
+                            source.CopyTo(dest);
+                        }
+                    }
+                }
+                return;
+            }
+            try
+            {
+                TID = ulong.Parse(args[1], NumberStyles.HexNumber);
+            }
+            catch
+            {
+                Console.WriteLine("The inputted TID is not valid");
+                return;
+            }
+
+            foreach (KeyValuePair<ulong, Title> kv in fs.Titles)
+            {
                 ulong titleId = kv.Key;
                 Title title = kv.Value;
 
@@ -52,7 +74,7 @@ namespace Quick_NCA_Finder
                     Console.WriteLine("Found!");
                     Console.WriteLine($"-->{titleId:X8}<--");
                     Console.WriteLine("Saving NCA to working directory...");
-                    DirectoryInfo titleRoot = new DirectoryInfo($"./{titleId:X8}");
+                    DirectoryInfo titleRoot = new DirectoryInfo($"./NCA/{titleId:X8}");
 
                     foreach (Nca nca in title.Ncas)
                     {
@@ -72,7 +94,7 @@ namespace Quick_NCA_Finder
                 {
                     Console.WriteLine($"{titleId:X8}");
                 }
-           }
+            }
             Console.WriteLine("TID not found!");
         }
     }
