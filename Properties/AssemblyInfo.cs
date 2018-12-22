af38ef9311745d98d3e4ac0ffe51a3327a570720
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-21 23:59:25 -0800

    Icon

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Quick NCA Finder")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2018 SunTheCourier")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
